5e8fb4a1dfb3493416eebe1c9b3ab5e228c01406
ChavaGlass <cglass102@gmail.com>
2016-02-16 22:23:02 -0500

    Create Fruit.java

---
+++ Project2/Fruit.java
@@ -0,0 +1,12 @@
+public class Fruit extends ProduceItem {
+	/**
+	 * Fruit constructor
+	 * @ param c code
+	 * @ param n name
+	 * @ param p price
+	 */
+	public Fruit(String c, String n, float p){
+		super(c, n, p);
+	}//constructor
+
+}//class
