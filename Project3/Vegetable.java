096e7d5ff48c17c0774e3eae2f397282702f840c
ChavaGlass <cglass102@gmail.com>
2016-02-16 22:31:59 -0500

    Create Vegetable.java

---
+++ Project3/Vegetable.java
@@ -0,0 +1,12 @@
+public class Vegetable extends ProduceItem{
+	
+	/**
+	 * Vegetable constructor
+	 * @ param c code
+	 * @ param n name
+	 * @ param p price
+	 */
+	public Vegetable(String c, String n, float p){
+		super(c,n,p);
+	}//constructor
+}//class
